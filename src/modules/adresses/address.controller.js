51c6df1640e316c838a2bbfd83b05fe1aa7c6046
Milad Shehata <miladshehata513@gmial.com>
2024-07-22 17:36:14 +0300

    update

--- src/modules/adresses/address.controller.js
+++ src/modules/adresses/address.controller.js
@@ -11,6 +11,27 @@ export const addAdress = catchAsyncErr(async (req, res, next) => {
     res.status(200).json({ "message": " success","statusCode":200 , result:result.adresses })
 })
 
+export const updateAddress = catchAsyncErr(async (req, res, next) => {
+    const { id } = req.params;
+    
+    const user = await userModel.findOneAndUpdate({ _id: req.user._id, "adresses._id": id },
+         {
+            $set: {
+                "adresses.$": req.body
+            }
+        },
+        { new: true }
+    );
+
+    if (!user) return next(new AppErr(`You are not authorized`, 404));
+
+    res.status(200).json({ 
+        "message": "Success", 
+        "statusCode": 200, 
+        "result": user.adresses 
+    });
+});
+
 export const removeAdress = catchAsyncErr(async (req, res, next) => {
     const result = await userModel.findByIdAndUpdate(req.user._id, {$pull:{adresses:{_id:req.body.address}}},{new:true})
     if (!result) return next(new AppErr(` you are not autherized`, 404))
--- src/modules/adresses/address.router.js
+++ src/modules/adresses/address.router.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { addAdress, getAllAdresses, removeAdress } from "./address.controller.js";
+import { addAdress, getAllAdresses, removeAdress, updateAddress } from "./address.controller.js";
 import { allowTo, protectedRoutes } from "../../middleware/protectedRoute.js";
 
 const addressRouter = express.Router()
 addressRouter.patch('/add-address',protectedRoutes,allowTo('user'),addAdress)
+addressRouter.put('/update-address/:id',protectedRoutes,allowTo('user'),updateAddress)
 addressRouter.delete('/remove',protectedRoutes,allowTo('user'),removeAdress)
 addressRouter.get('/get-addresses',protectedRoutes,allowTo('user'),getAllAdresses)
 
