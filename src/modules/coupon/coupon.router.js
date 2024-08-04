a6ac0f0ac85281747b71fdc50aa13da30ab8d886
Milad Shehata <miladshehata513@gmial.com>
2024-08-04 18:50:20 +0300

    origin

--- src/modules/coupon/coupon.router.js
+++ src/modules/coupon/coupon.router.js
@@ -5,7 +5,7 @@ import { allowTo, protectedRoutes } from "../../middleware/protectedRoute.js";
 const couponRouter = express.Router()
 
 couponRouter.post('/create-coupon',protectedRoutes,allowTo('admin'),createCopon)
-couponRouter.get('/get-all-coupon',allowTo('admin'),getAllCopons)
+couponRouter.get('/get-all-coupon',protectedRoutes,allowTo('admin'),getAllCopons)
 couponRouter.get('/get-coupon/:id',getCopon)
 couponRouter.put('/update-coupon/:id',protectedRoutes,allowTo('admin'),updateCopon)
 couponRouter.delete('/delete-coupon/:id',protectedRoutes,allowTo('admin'),deleteCopon)
