c6cf960cf06348cbd435294d9a64086f16234d78
Milad Shehata <miladshehata513@gmial.com>
2024-08-26 15:14:30 +0300

    origin

--- src/modules/notification/notification.route.js
+++ src/modules/notification/notification.route.js
@@ -7,7 +7,7 @@ import { getNotSchema } from './notification.validation.js';
 
 const notificationRouter = express.Router();
 
-notificationRouter.post("/create-notifications", protectedRoutes, allowTo('admin'), createNotifications);
+notificationRouter.post("/create-notification", protectedRoutes, allowTo('admin'), createNotifications);
 notificationRouter.get("/get-all-notifications", protectedRoutes, allowTo('user','admin','waiter'), getNotifications);
 notificationRouter.put("/update-notification/:id", protectedRoutes, allowTo('user','admin','waiter'),  validation(getNotSchema), updateNotification);
 notificationRouter.get("/get-notification/:id", protectedRoutes, allowTo('user','admin','waiter'), validation(getNotSchema), getOneNotification);
