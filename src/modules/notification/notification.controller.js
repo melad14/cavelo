acd8d4d13a9cac7993eb691ec522f980bba53127
Milad Shehata <miladshehata513@gmial.com>
2024-08-27 17:57:26 +0300

    signal

--- src/modules/notification/notification.controller.js
+++ src/modules/notification/notification.controller.js
@@ -2,29 +2,20 @@ import { notificationModel } from "../../../databases/models/notifcation.js";
 import { AppErr } from "../../utils/AppErr.js";
 import { catchAsyncErr } from "../../utils/catcherr.js";
 import cron from 'node-cron';
-import Pusher from 'pusher';
+
 import { sendNotificationToAll } from "./oneSignalPushNotification.js";
 
-const pusher = new Pusher({
-    appId: "1832769",
-    key: "74fa23b5f9fdd3fa37f0",
-    secret: "c59f35157bcebbfb400a",
-    cluster: "eu",
-    useTLS: true
-  });
-  
-  
-  
 
 export const createNotifications = catchAsyncErr(async (req, res, next) => {
     const{title,message}=req.body
-    notid='admin'
+    let notid='admin'
     const notification = new notificationModel({title,message,notid});
     await notification.save()
     await sendNotificationToAll(title,message)
     res.status(200).json({ "message": " success", "statusCode":200 ,notification })
 
 });
+
 export const getNotifications = catchAsyncErr(async (req, res, next) => {
 
     const notifications = await notificationModel.find().sort({ createdAt: -1 });
