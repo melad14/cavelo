79c580f4fc7659ad6e345c9d4195aff31f528f0e
Milad Shehata <miladshehata513@gmial.com>
2024-08-27 19:51:57 +0300

    signal

--- src/modules/admin-auth/admin.validiation.js
+++ src/modules/admin-auth/admin.validiation.js
@@ -4,12 +4,13 @@ import Joi from 'joi';
 const egyptianPhoneNumberPattern = /^20(1[0-9]{9})$/;
 
 export const signInSchema = Joi.object({
-  phone: Joi.string().pattern(egyptianPhoneNumberPattern).required()
+  phone: Joi.string().pattern(egyptianPhoneNumberPattern).required(),
+  subscriptionId:Joi.string(),
 });
 
 export const verifySchema = Joi.object({
     phone: Joi.string().pattern(egyptianPhoneNumberPattern).required(),
     otp: Joi.string().length(6).required(),
-    subscriptionId:Joi.string(),
+  
   });
   
